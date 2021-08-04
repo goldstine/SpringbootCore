fe2142a47cb47b7e02100fa8998ee314313e41be
刘磊 <15797899690@163.com>
2021-08-04 16:15:06 +0800

    first commit

---
+++ springboot-01-cache/src/main/java/com/atguigu/cache/bean/Department.java
@@ -0,0 +1,8 @@
+package com.atguigu.cache.bean;
+
+
+public class Department {
+    private Integer id;
+    private String departmentName;
+
+}
---
+++ springboot-01-cache/src/test/java/com/atguigu/cache/Springboot01CacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01CacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
